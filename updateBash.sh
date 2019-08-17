52b796ca6b9aab6e715ba92124c35ace4c92401a
quarenw <quaren@gmail.com>
2019-08-17 23:47:35 +0100

    Added bash stuffs

---
+++ updateBash.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -d ${HOME}/.bash_it ]; then 
+  echo THERE IS BASH_IT
+else 
+  cp -r ${HOME}/.quaren/bash_it ${HOME}/.bash_it
+  ~/.bash_it/install.sh
+  [ -f ${HOME}/.bashrc ] && sed -i.backup -e "s,THEME='.*',THEME='zork',gi" ${HOME}/.bashrc
+  [ -f ${HOME}/.bash_profile ] && sed -i.backup -e "s,THEME='.*',THEME='zork',gi" ${HOME}/.bash_profile
+  [ -f ${HOME}/.bashrc ] && source ${HOME}/.bashrc
+  [ -f ${HOME}/.bash_profile ] && source ${HOME}/.bash_profile
+  bash_it update
+  echo bash_it installed and updated
+fi
+
