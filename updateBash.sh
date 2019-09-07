c7817da7bda31e273f0d4d5d820dc2ce41b7cb9d
quaren <quaren@quarens-MacBook-Pro.local>
2019-09-07 12:12:49 +0100

    Fix updateBash

--- updateBash.sh
+++ updateBash.sh
@@ -5,11 +5,11 @@ if [ -d ${HOME}/.bash_it ]; then
 else 
   cp -r ${HOME}/.quaren/bash_it ${HOME}/.bash_it
   ~/.bash_it/install.sh
-  [ -f ${HOME}/.bashrc ] && sed -i.backup -e "s,THEME='.*',THEME='zork',gi" ${HOME}/.bashrc
-  [ -f ${HOME}/.bash_profile ] && sed -i.backup -e "s,THEME='.*',THEME='zork',gi" ${HOME}/.bash_profile
-  [ -f ${HOME}/.bashrc ] && source ${HOME}/.bashrc
-  [ -f ${HOME}/.bash_profile ] && source ${HOME}/.bash_profile
-  bash_it update
-  echo bash_it installed and updated
+  [ -f ${HOME}/.bashrc ] && sed -i.backup -e "s,THEME='.*',THEME='zork',g" ${HOME}/.bashrc
+  [ -f ${HOME}/.bash_profile ] && sed -i.backup -e "s,THEME='.*',THEME='zork',g" ${HOME}/.bash_profile
+  # [ -f ${HOME}/.bashrc ] && source ${HOME}/.bashrc
+  # [ -f ${HOME}/.bash_profile ] && source ${HOME}/.bash_profile
+  # bash_it update
+  echo bash_it installed updated
 fi
 
