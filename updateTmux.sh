89674257f9620a10c2a36dc55b82c8faffb01a89
quaren <quaren@quarens-MacBook-Pro.local>
2019-09-07 13:13:33 +0100

    Update tmux shell to copy plugins

--- updateTmux.sh
+++ updateTmux.sh
@@ -3,5 +3,6 @@
 
  [ -f ${HOME}/.tmux.conf ] && mv ${HOME}/.tmux.conf ${HOME}/.tmux.conf.bak
  cp ${HOME}/.quaren/files/tmux.conf ${HOME}/.tmux.conf
+ cp -r ${HOME}/.quaren/files/tmux/ ${HOME}/.tmux/
 
 
