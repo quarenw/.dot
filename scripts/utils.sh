add6a20db992b92dc1fd42c7f77b8f4019d4e777
quaren <quaren@gmail.com>
2024-03-04 23:24:21 +0000

    stage: fix ppa check again

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -82,15 +82,15 @@ install_pac() {
         if [ ! -z ${PPA} ]; then
           if command_exists add-apt-repository; then
             if ask "Install PPA support?" Y; then
-              msg "Skipping"
-            else
-              yes | sudo apt-get install software-properties-common
               sudo add-apt-repository -y ppa:$2
               sudo apt-get update
               yes | sudo apt-get install $1
               msg "Installled $1"
+            else
+              msg "Skipping"
             fi
           else
+              yes | sudo apt-get install software-properties-common
               sudo add-apt-repository -y ppa:$2
               sudo apt-get update
               yes | sudo apt-get install $1
