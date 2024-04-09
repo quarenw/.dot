17566d775882dfb4bcbaea30424897e0830c031f
quaren <quaren@gmail.com>
2024-04-09 19:33:48 +0100

    stage: fixes

--- files/tmux_custom/mouse.sh
+++ files/tmux_custom/mouse.sh
@@ -1,6 +1,6 @@
 show_mouse() {
 	local index=$1
-	local icon="$(get_tmux_option "@catppuccin_test_icon" "\udb80\udf7d")"
+	local icon="$(get_tmux_option "@catppuccin_test_icon" "󰍽")"
 	local color="$(get_tmux_option "@catppuccin_test_color" "$thm_green")"
 
 	local module="$( build_status_module "$index" "$icon" "$color" "#( tmux show -gw mouse | sed -e 's/mouse //g' | tr "[o]" "[O]" )" )"
--- scripts/core_cli/configTmux.sh
+++ scripts/core_cli/configTmux.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -e
+# No exit on error as we are using test for symlinks
 . ${HOME}/.dot/scripts/utils.sh
 
 if [ -f ${HOME}/.tmux.conf ]; then
@@ -11,13 +11,21 @@ if [ -f ${HOME}/.tmux.conf ]; then
 fi
 
 ln -sf ${HOME}/.dot/files/tmux.conf ${HOME}/.tmux.conf
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+if ask "Install TPM?" Y; then
+  [ -d ${HOME}/.tmux/plugins/tpm ] && rm -r ${HOME}/.tmux/plugins/tpm
+  git clone https://github.com/tmux-plugins/tpm ${HOME}/.tmux/plugins/tpm
+fi
 
 mkdir -p ${HOME}/.tmux/plugins/tmux/
 if [ -d ${HOME}/.tmux/plugins/tmux/custom ]; then
-	if [ ! test -L ~/.tmux/plugins/tmux/custom ]; then
+  IS_SYMBOLIC=$(test -L ${HOME}/.tmux/plugins/tmux/custom && echo "yes")
+	if [ -z $IS_SYMBOLIC ]; then
   	# Move the folder if it isn't a symlink
-    mv -f ${HOME}/.tmux/plugins/tmux/custom ${HOME}/.dot/backup/
+    if ask "Backup custom folder?" Y; then
+      mv -f ${HOME}/.tmux/plugins/tmux/custom ${HOME}/.dot/backup/
+    else
+      rm -r ${HOME}/.tmux/plugins/tmux/custom
+    fi
   else
   	# If it is a symlink then burn it with fire I guess
     rm ${HOME}/.tmux/plugins/tmux/custom
